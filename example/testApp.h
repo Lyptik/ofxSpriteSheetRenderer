6cfb7968259fb03d7def98ac5f3aca38a9c06ed6
Tom Duchêne <tom@theoriz.com>
2015-04-08 14:14:58 +0200

    Update the example to add the pingpong feature

--- example/testApp.h
+++ example/testApp.h
@@ -17,7 +17,8 @@ static animation_t walkAnimation =
 	0,	/* .nexttick		(unsigned int) - the next time the frame should change. based on frame duration. This is an internal variable and should always be set to 0 at init	*/
 	-1,	/* .loops			(int) - the number of loops to run. -1 equals infinite. This can be adjusted at runtime to pause animations, etc.									*/
 	-1,	/* .finalindex		(int) - the index of the final tile to draw when the animation finishes. -1 is the default so total_frames-1 will be the last frame.				*/
-	1	/* .frameskip		(int) - the incrementation of each frame. 1 should be set by default. If you wanted the animation to play backwards you could set this to -1, etc.	*/
+	1,	/* .frameskip		(int) - the incrementation of each frame. 1 should be set by default. If you wanted the animation to play backwards you could set this to -1, etc.	*/
+	false	/* .pingpong		(bool) - the boolean defining if the animation should go back and forth when reaching the first and last frames. "false" is the default  */
 };
 
 //a quick and dirty sprite implementation
